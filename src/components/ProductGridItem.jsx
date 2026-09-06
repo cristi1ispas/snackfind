d8c451a91817caf74b446ac8ca9d8feeb3da663b
Cristi <cristi1ispas@tutanota.com>
2026-09-06 14:29:08 +0300

    small tweaks

--- src/components/ProductGridItem.jsx
+++ src/components/ProductGridItem.jsx
@@ -28,8 +28,8 @@ function ProductGridItem({ product, selectProduct }) {
         <md-icon>release_alert</md-icon>
         <span>Limited!</span>
       </div>)}
-      <md-icon-button className="productFavBtn" toggle >
-        <md-icon className="material-symbols-outlined">favorite</md-icon>
+      <md-icon-button className="productFavBtn" toggle disabled>
+        <md-icon>favorite</md-icon>
         <svg slot='selected' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Z"/></svg>
       </md-icon-button>
       <md-divider onClick={() => selectProduct(product)} />
@@ -38,7 +38,7 @@ function ProductGridItem({ product, selectProduct }) {
         <sup>99</sup>
         <md-icon>percent_discount</md-icon>
       </div>
-      <md-outlined-icon-button className="addToBasketBtn" toggle >
+      <md-outlined-icon-button className="addToBasketBtn" toggle disabled>
         <md-icon>add_shopping_cart</md-icon>
         <md-icon slot="selected">check</md-icon>
       </md-outlined-icon-button>
