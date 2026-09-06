ed666a46fff43c16488532938e9d54f233c7b108
Cristi <cristi1ispas@tutanota.com>
2026-09-06 15:50:38 +0300

    shop lists for prices, might change

--- src/components/ProductPageLayout.jsx
+++ src/components/ProductPageLayout.jsx
@@ -62,58 +62,68 @@ function ProductPage({ product, isOpen, onClose }) {
             Add to cart
           </md-outlined-button>
         </div>
+        <md-divider />
         <md-list className="productShopsList">
-          <md-list-item className="productShopCard" type="link">
-            <img
-              slot="start"
-              src="https://static.mega-image.ro/medias/sys_master/products/h68/h99/9541402787870.jpg"
-            />
-            <div slot="headline">Potica</div>
-            <div slot="supporting-text">Maria Eliza SRL</div>
-            <div slot="end" className="productPrice">
-              <div>
-                <md-icon>shelves</md-icon>
-                <span>19</span>
-                <sub>.99</sub>
-              </div>
-              <div className="dateUpdated">24/02</div>
+          <div className="productShopCard">
+            <div className="productShopHeadline">
+              <md-list-item className="shop-button" type="button">
+                <img slot="start" src='public/Shop-photo-id-1.png' />
+                <div slot="headline">Potica</div>
+                <div slot="supporting-text">Maria Eliza SRL</div>
+              </md-list-item>
+              <md-list-item className="productPriceButton" type="button">
+                <div className="productPrice">
+                  <div className='productPriceNumeral'>
+                    <md-icon>shelves</md-icon>
+                    <span>19</span>
+                    <sub>.99</sub>
+                  </div>
+                  <div className="dateUpdated">24/02</div>
+                </div>
+              </md-list-item>
             </div>
-          </md-list-item>
-          <md-list-item className="productShopCard" type="link">
-            <img
-              slot="start"
-              src="https://static.mega-image.ro/medias/sys_master/products/h68/h99/9541402787870.jpg"
-            />
-            <div slot="headline">Potica</div>
-            <div slot="supporting-text">Maria Eliza SRL</div>
-            <div slot="end" className="productPrice">
-              <div>
-                <md-icon>point_of_sale</md-icon>
-                <span>19</span>
-                <sub>.99</sub>
-              </div>
-              <div className="dateUpdated">24/02</div>
+          </div>
+          <md-divider />
+          <div className="productShopCard">
+            <div className="productShopHeadline">
+              <md-list-item className="shop-button" type="button">
+                <img slot="start" src='public/Shop-photo-id-1.png' />
+                <div slot="headline">Potica</div>
+                <div slot="supporting-text">Maria Eliza SRL</div>
+              </md-list-item>
+              <md-list-item className="productPriceButton" type="button">
+                <div className="productPrice">
+                  <div className='productPriceNumeral'>
+                    <md-icon>cloud_download</md-icon>
+                    <span>19</span>
+                    <sub>.99</sub>
+                  </div>
+                  <div className="dateUpdated">24/02</div>
+                </div>
+              </md-list-item>
             </div>
-          </md-list-item>
-          <md-list-item className="productShopCard" type="link">
-            <img
-              slot="start"
-              src="https://static.mega-image.ro/medias/sys_master/products/h68/h99/9541402787870.jpg"
-            />
-            <div slot="headline">Potica</div>
-            <div slot="supporting-text">Maria Eliza SRL</div>
-            <div slot="end" className="productPrice">
-              <div>
-                <md-icon>cloud_download</md-icon>
-                <span>19</span>
-                <sub>.99</sub>
-              </div>
-              <div className="dateUpdated">24/02</div>
+          </div>
+          <md-divider />
+          <div className="productShopCard">
+            <div className="productShopHeadline">
+              <md-list-item className="shop-button" type="button">
+                <img slot="start" src='public/Shop-photo-id-1.png' />
+                <div slot="headline">Potica</div>
+                <div slot="supporting-text">Maria Eliza SRL</div>
+              </md-list-item>
+              <md-list-item className="productPriceButton" type="button">
+                <div className="productPrice">
+                  <div className='productPriceNumeral'>
+                    <md-icon>point_of_sale</md-icon>
+                    <span>19</span>
+                    <sub>.99</sub>
+                  </div>
+                  <div className="dateUpdated">24/02</div>
+                </div>
+              </md-list-item>
             </div>
-          </md-list-item>
-          <md-text-button>
-            <u>Show All</u>
-          </md-text-button>
+          </div>
+          <md-divider />
         </md-list>
       </div>
     </div>
